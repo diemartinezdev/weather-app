f690a071bf5f5af033057a81b894487f43682124
Diego Martinez <martinez.diego90@gmail.com>
2023-06-02 20:28:35 +0200

    add changes

---
+++ src/app/weather.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getweather(city: string) { 
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=37dfaef5356ec034e065327cdd3b0093&units=metric')
+  }
+}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
+
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
-export class WeatherComponent {
+export class WeatherComponent implements OnInit {
+  myWeather: any;
+  temperature: number = 0;
+  feelsLikeTem: number = 0;
+  humidity: number = 0;
+  pressure: number = 0;
+  summary: string = '';
+  iconURL: string = '';
+  city: string = 'Montevideo';
 
-}
+
+  constructor(private weatherService: WeatherService) { }
+  
+  ngOnInit(): void {
+    this.weatherService.getweather(this.city).subscribe({
+
+      next: (res) => {
+        this.myWeather = res;
+        console.log(this.myWeather);
+        this.temperature = this.myWeather.main.temp;
+        this.feelsLikeTem = this.myWeather.main.feels_like;
+        this.humidity = this.myWeather.main.humidity;
+        this.pressure = this.myWeather.main.pressure;
+        this.summary = this.myWeather.weather[0].main;
+        this.city = this.myWeather.name;
+        this.iconURL = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png';
+      },
+
+      error: (error) => console.log(error.message),
+      complete: () => console.log('API call completed')
+    })
+  }
+  }
