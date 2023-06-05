1663ff0cb60f8ccb4062c0c9efb00d3c317af229
Diego Martinez <martinez.diego90@gmail.com>
2023-06-05 20:34:28 +0200

    add little modifications

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,6 +9,6 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getweather(city: string) { 
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=37dfaef5356ec034e065327cdd3b0093&units=metric')
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=<apiKey>&units=metric')
   }
 }
